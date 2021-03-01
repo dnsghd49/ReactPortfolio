d7254d9a60e0c51a11114b494c253efa6eb99b8c
Aaron Lu <aaronnoh0409@gmail.com>
2021-03-01 01:26:44 -0500

    working on nav

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 import Main from "./pages/main.js";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from "./components/Navbar/index.js";
 //import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-          {/* <Navigation /> */}
+          <Navbar />
           <Switch>
             <Route exact path="/"
               component={Main} />
+            <Route exact path="/main" component={Main} />
           </Switch>
       </div>
     </BrowserRouter>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,37 @@
 import React from "react";
 import "./style.css";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+// Depending on the current path, this component sets the "active" className on the appropriate navigation link item
 function Navbar() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="navbar-brand">
-        Employee Directory
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar-expand-md tm-navbar" id="tmNav">
+            <div className="container">
+                <div className="tm-next">
+                    <p className="navbar-brand">Hello</p>
+                </div>
+
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i className="fas fa-bars navbar-toggler-icon"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                    { /*  
+                        <li className="nav-item">
+                            <a className="nav-link tm-nav-link" href="#introduction">Introduction</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link tm-nav-link" href="#work">Our Work</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link tm-nav-link" href="#contact">Contact Us</a>
+                        </li>
+*/}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
