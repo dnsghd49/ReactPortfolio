288cb612ca513eecc78bc47e6df91ce2be660802
Aaron Lu <aaronnoh0409@gmail.com>
2021-03-02 20:14:03 -0500

    broken

---
+++ src/components/Me/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import me from './me.png'; // Tell webpack this JS file uses this image
+import './style.css';
+
+console.log(me); // /logo.84287d09.png
+
+function Me() {
+  console.log(me);
+  return(
+      <div>
+    <img src={me} alt="me" />
+    </div>
+  );
+}
+
+export default Me;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import Me from "../components/Me"
 
 class About extends Component {
   render() {
     return (
       <div className="abground">
+
         <p className="abpa">Aaron's Github Page</p>
         <p class="contain">
           Here you can see Ayron's project through Github his page! those projects are not so great, normal looking projects so don't expect some crazy magicall stuff xD
@@ -14,6 +16,7 @@ class About extends Component {
           <a className="nav-link tm-nav-link" href="https://github.com/dnsghd49">
             <button type="submit" className="githubBtn" href="https://github.com/dnsghd49">GitHub</button>
           </a>
+          <Me />
         </div>
       </div>
     );
