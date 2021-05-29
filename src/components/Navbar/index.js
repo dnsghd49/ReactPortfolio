e21c82a1b7e2dd0d16b7a1fc8acfc29a94f8f7f7
Aaron Lu <aaronnoh0409@gmail.com>
2021-05-29 00:50:10 -0400

    new nav bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,21 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-md tm-navbar">
-            <div className="container">
+        <nav>
+            <ul>
+                <li>
                 <Link className="navbar-brand" to="/">Main</Link>
-
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <Link to="/about" className="nav-link tm-nav-link">About</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/idkyet" className="nav-link tm-nav-link">Projects</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+                </li>
+                <li>
+                <Link to="/idkyet" className="nav-link tm-nav-link">Projects</Link>
+                </li>
+            </ul>
         </nav>
     );
 }
