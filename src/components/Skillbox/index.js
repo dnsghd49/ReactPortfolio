871773a31c3383acc37aca3e2a70a2af145348af
Aaron Lu <aaronnoh0409@gmail.com>
2021-05-29 01:07:34 -0400

    setting up skill box

---
+++ src/components/Skillbox/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Skillbox () {
+    return (
+        <div></div>
+    );
+}
+
+export default Skillbox;
