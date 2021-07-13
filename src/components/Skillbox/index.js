caa0c197859de599617f304a1420a9a0359def55
Aaron Lu <aaronnoh0409@gmail.com>
2021-07-13 11:47:16 -0400

    layout for skill box is ready

--- src/components/Skillbox/index.js
+++ src/components/Skillbox/index.js
@@ -3,12 +3,30 @@ import { Link } from "react-router-dom";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import javapic from './img/javascript.png';
+import reactpic from './img/react.png';
+import nodepic from './img/node.png'
 
-function Skillbox () {
+
+function Skillbox() {
     return (
-        <div>
-            <img src={javapic} alt="BigCo Inc. logo"/>
-        </div>
+        <section className="skills">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm">
+                        <div className="skills-card cardground">
+                            <div className="skills-card-body card-body">
+                                <h2 id="skills" class="card-subtitle mt-3">Technical Skills</h2>
+                                <div className="mx-auto" id="skill-icons">
+                                    <img src={javapic} alt="JS logo" />
+                                    <img src={reactpic} alt="React logo" />
+                                    <img src={nodepic} alt="React logo" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
